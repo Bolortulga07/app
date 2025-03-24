14c2bff8b333b8ed0d25cd35efadd9dfea3eecb1
bolortulga10 <bolortulga07@gmail.com>
2025-03-24 17:36:52 +0800

    barag bolson

--- src/modules/transaction/graphql/transactionQueries.ts
+++ src/modules/transaction/graphql/transactionQueries.ts
@@ -1,4 +1,4 @@
-import { SortOrder } from "mongoose";
+import { FilterQuery, SortOrder } from "mongoose";
 import { Transactions } from "../model/transactionModel";
 
 export const transactionQueriesTypeDefs = `
@@ -12,15 +12,47 @@ export const transactionQueriesTypeDefs = `
 
 type TransactionFilter = {
   categoryId?: string;
+  limit?: number;
+  skip?: number;
+  page?: number;
+  sortBy?: string;
+  sortOrder?: SortOrder;
+  type?: string;
+  categoryIds?: string[];
+  sortByDate?: string;
+  orderByDate?: SortOrder;
+  minAmount?: number;
+  maxAmount?: number;
 };
 
-const generateFilter = (args: TransactionFilter) => {
-  const { categoryId } = args;
+const generateFilter = (args: any) => {
+  const {
+    categoryId,
+    limit = 20,
+    page = 1,
+    skip = (page - 1) * limit,
+    sortBy = "createdBy",
+    sortOrder = -1,
+    type = "income",
+    categoryIds = [],
+    sortByDate = "createdBy",
+    orderByDate = -1,
+    minAmount,
+    maxAmount,
+  } = args;
 
-  const filter: TransactionFilter = {};
+  const filter: FilterQuery<TransactionFilter> = {};
 
   if (categoryId) {
     filter["categoryId"] = categoryId;
+  }
+
+  if (categoryIds.length) {
+    filter["categoryId"] = { $in: categoryIds };
+  }
+
+  if (type) {
+    filter["type"] = { $eq: type };
   }
 
   return filter;
@@ -54,12 +86,6 @@ export const transactionQueries = {
       skip = (page - 1) * limit,
       sortBy = "createdBy",
       sortOrder = -1,
-      type = "income",
-      categoryIds = [],
-      sortByDate = "createdBy",
-      orderByDate = -1,
-      minAmount,
-      maxAmount,
     } = args;
 
     const filter: TransactionFilter = generateFilter(args);
